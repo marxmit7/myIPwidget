997590f663c48b8a9b68b7aa68d9d3efd2a84947
pymit <amit2rockpower@gmail.com>
2020-07-16 19:31:36 +0530

    shows phone's ip address

---
+++ app/src/main/java/com/example/ip/NewAppWidget.java
@@ -0,0 +1,80 @@
+package com.example.ip;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+    private static final String ACTION_SIMPLEAPPWIDGET = "ACTION_BROADCASTWIDGETSAMPLE";
+    private String ipAdd = "before";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        Intent intent = new Intent(context, NewAppWidget.class);
+        intent.setAction(ACTION_SIMPLEAPPWIDGET);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (ACTION_SIMPLEAPPWIDGET.equals(intent.getAction())) {
+
+            ipAdd = "amit";
+            try {
+                getIp();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            views.setTextViewText(R.id.appwidget_text, ipAdd);
+            // This time we dont have widgetId. Reaching our widget with that way.
+            ComponentName appWidget = new ComponentName(context, NewAppWidget.class);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidget, views);
+        }
+    }
+
+    private void getIp() {
+        ipAdd=Utils.getIPAddress(true);
+
+        Log.i("ip: ", "" + ipAdd );
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
